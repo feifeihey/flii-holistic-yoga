7419d6cc486b5b80e59bcca70aadaf6f4b4d70e3
feifeihey <feifei.li.email@gmail.com>
2026-05-22 17:24:59 +0800

    Add files via upload

--- base-path.js
+++ base-path.js
@@ -1,7 +1,12 @@
 (function () {
   var path = window.location.pathname;
-  if (/\.html$/i.test(path)) path = path.replace(/[^/]+$/, "");
+  if (/\.html$/i.test(path)) {
+    path = path.replace(/[^/]+$/, "");
+  } else if (path !== "/" && !path.endsWith("/")) {
+    path = path.replace(/[^/]+$/, "");
+  }
   if (!path.endsWith("/")) path += "/";
+  window.__siteRoot = path;
   var head = document.getElementsByTagName("head")[0];
   if (!head || head.querySelector("base[data-site-base]")) return;
   var base = document.createElement("base");
